96c4204d02d0a6d2f6f314461619e1ba038bcfae
pycoder74 <105924451+pycoder74@users.noreply.github.com>
2023-11-27 08:58:54 +0000

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,34 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 import os
-
+from nltk.tokenize import word_tokenize
+os.system('pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org nltk')
 url = 'https://github.com/pycoder74/eTasks'
-base_raw_url = 'https://raw.githubusercontent.com'
+ext = 'iso'
 
 def listFD(url, ext=''):
-    page = requests.get(url, verify=False).text
+    page = requests.get(url, verify = False).text
     soup = BeautifulSoup(page, 'html.parser')
-    return [base_raw_url + url.replace('https://github.com', '') + '/' + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
-
-directories = listFD(url, ext='.py')
-
-needed_files = []
-
-# Check for the presence of '.py' in each directory
-for directory in directories:
-    needed_files.append(directory)
-
-print(f"Files to download: {needed_files}")
-
-# Download files
-for file_url in needed_files:
-    response = requests.get(file_url)
-    
-    if response.status_code == 200:
-        # Generate a unique filename with a timestamp
-        filename = os.path.basename(file_url)
-        print(f'Downloading file from {file_url}')
-        with open(filename, mode="wb") as file:
-            file.write(response.content)
+    return [url + '/' + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)]
+directories =listFD(url)
+list = []
+for i in directories:
+    list.append(i)
+    tokenized_input = word_tokenize(i)
+    if 'blob' in tokenized_input[2]:
+        print(tokenized_input[2])
     else:
-        print(f'Failed to download file from {file_url}. Status code: {response.status_code}')
+        pass
+    
+    
